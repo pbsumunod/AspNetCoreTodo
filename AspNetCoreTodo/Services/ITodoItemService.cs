156c9a67cfb24726eaebcd9ccdbc1a31bb9f5f84
Pamela Sumunod <pamela.sumunod@qnet.net>
2020-02-24 17:57:25 +0800

    Adding an item and saving to dabase; Action upon checkbox select

---
+++ AspNetCoreTodo/Services/ITodoItemService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Services
+{
+    public interface ITodoItemService
+    {
+        Task<TodoItem[]> GetIncompleteItemsAsync();
+        Task<bool> AddItemAsync(TodoItem newItem);
+    }
+}
